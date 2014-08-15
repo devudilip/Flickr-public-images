4ce836bfd73dfa027a3206bdc45e02b27d3a14c1
devudilip <devu.dilip@gmail.com>
2014-08-15 18:15:27 +0530

    un wanted code

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -22,14 +22,4 @@ class PhotosController < ApplicationController
    @results = JSON.parse(@json_hash)
  end
 
- def friends_photos_public
-  contact_id = params[:id]
-  contact_results =  open("https://api.flickr.com/services/rest/?method=flickr.contacts.getPublicList&&api_key=533e7b41f75718c84c94dad1699f740d&&page=10&&per_page=10&&format=json&&nojsoncallback=1&&user_id=#{params[:id]}").read
-  if JSON.parse(contact_results)['contacts']['contact']
-    @contacts = JSON.parse(contact_results)['contacts']['contact'].map{ |i, arr| arr = i['nsid'] }
-    @jsonresults =  open("http://api.flickr.com/services/feeds/photos_public.gne?format=json&&ids=#{@contacts.join(",")}").read
-    @json_hash = JSON.load(@jsonresults.to_json).gsub("jsonFlickrFeed(", "").gsub(")", "")
-    @results = JSON.parse(@json_hash)
-  end
-end
 end
