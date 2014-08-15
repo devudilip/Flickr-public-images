357183274752e33b2432aa59e0ec388923a5c6a4
devudilip <devu.dilip@gmail.com>
2014-08-15 18:23:05 +0530

    code clean

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,2 +1,10 @@
 module PhotosHelper
+
+  def get_clean_url description
+    description.split("<img src=").last.split(".jpg").first.gsub("\"", "")
+  end
+
+  def get_author name
+    name.gsub("nobody@flickr.com (", "") 
+  end
 end
