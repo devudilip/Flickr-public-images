e6f37d3d9cd56fbff700589328b15fb58e5c6ed0
devudilip <devu.dilip@gmail.com>
2014-08-15 18:49:27 +0530

    DRY code

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,20 +4,25 @@ class PhotosController < ApplicationController
 
   def index
     @jsonresults =  open("http://api.flickr.com/services/feeds/photos_public.gne?format=json").read
-    @json_hash = JSON.load(@jsonresults.to_json).gsub("jsonFlickrFeed(", "").gsub(")", "")#.first
-    @results = JSON.parse(@json_hash)
+    get_parsed_results
   end
 
   def show
     author_id = params[:id]
     @jsonresults =  open("http://api.flickr.com/services/feeds/photos_public.gne?format=json&&id=#{author_id}").read
-    @json_hash = JSON.load(@jsonresults.to_json).gsub("jsonFlickrFeed(", "").gsub(")", "")
-    @results = JSON.parse(@json_hash)
+    get_parsed_results
   end
 
   def friends_photos
    contact_id = params[:id]
    @jsonresults =  open("https://api.flickr.com/services/feeds/photos_friends.gne?user_id=#{contact_id}&&format=json&&friends=1&&&&nojsoncallback=1").read
+   get_parsed_results
+ end
+
+
+ private
+
+ def get_parsed_results
    @json_hash = JSON.load(@jsonresults.to_json).gsub("jsonFlickrFeed(", "").gsub(")", "")
    @results = JSON.parse(@json_hash)
  end
